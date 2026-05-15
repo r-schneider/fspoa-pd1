f9a11f111e378755200075680c79a639673bc894
RODRIGO MONTE SCHNEIDER <01549425021@senacrs.edu.br>
2026-05-14 21:19:28 -0300

    fix: product entity

--- app/models/product.py
+++ app/models/product.py
@@ -3,19 +3,18 @@ from app.core.database import Base
 from app.enums.unit_measure_enum import UnitMeasure
 
 class Product(Base):
-    __tablename__ = "produtos"
+    __tablename__ = "PRODUTOS"
 
-    id = Column(Integer, primary_key=True, index=True)  
-    nome = Column(String, nullable=False)
-    descricao = Column(String)  
-    codigo_barras = Column(String, unique=True)
-    sku = Column(String, unique=True)  
-    estoque_atual = Column(Integer, nullable=False, default=0) 
-    estoque_minimo = Column(Integer, nullable=False, default=0)  
-    valor_venda  = Column(Numeric(10,2), nullable=False)
-    marca = Column(String)
-    unidade_medida = Column(SqlEnum(UnitMeasure), nullable=False)
-    ativo = Column(Boolean, default=True)  
-    criado_em = (DateTime)
-    atualizado_em = (DateTime)
-    categoria_id = Column(Integer)
+    id = Column("ID", Integer, primary_key=True, index=True)  
+    name = Column("NOME", String, nullable=False)
+    description = Column("DESCRICAO", String)  
+    barcode = Column("CODIGO_BARRAS", String, unique=True)
+    sku = Column("SKU", String, unique=True)  
+    current_stock = Column("ESTOQUE_ATUAL", Integer, nullable=False, default=0) 
+    minimum_stock = Column("ESTOQUE_MINIMO", Integer, nullable=False, default=0)  
+    price  = Column("PRECO", Numeric(10,2), nullable=False)
+    unit_measure = Column("UNIDADE_MEDIDA", SqlEnum(UnitMeasure), nullable=False)
+    active = Column("ATIVO", Boolean, default=True)  
+    created_at = ("DATA_CRIACAO", DateTime)
+    updated_at = ("DATA_ATUALIZACAO", DateTime)
+    category_id = Column("ID_CATEGORIA", Integer)
